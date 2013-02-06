adc149e8cb1b6a6c62ddd4716bf5288f9c385956
Oleg Bulatov <oleg@bulatov.me>
2013-02-06 23:59:15 +0400

    Basic b-axis implementation
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- b-axis.js
+++ b-axis.js
@@ -1,12 +1,32 @@
 (function(Vis) {
+"use strict";
 
 Vis.blocks['b-axis'] = {
     init: function($object) {
         this.$object = $object;
     },
 
-    update: function() {
-        this.$object.html('Hello, world!');
+    update: function(position, ticks) {
+        var offsets = {
+            left: 'top', right: 'top',
+            top: 'left', bottom: 'left'
+        };
+
+        var content = '';
+        var offset = offsets[position] || 'top';
+        for (var i = 0; i < ticks.length; ++i) {
+            var tick = ticks[i];
+            content += Vis.render({
+                cls: 'b-axis__value',
+                attrs: { style: offset + ':' + tick.offset + 'px' },
+                content: [
+                    { cls: 'b-axis__label', content: tick.label + (tick.extraLabel ? '<br>' + tick.extraLabel : '') },
+                    { cls: 'b-axis__tick' }
+                ]
+            });
+        }
+        this.$object.attr('class', 'b-axis b-axis_pos_' + position);
+        this.$object.html(content);
     }
 };
 
--- core.js
+++ core.js
@@ -1,6 +1,11 @@
 (function(exports, undefined) {
 "use strict";
 
+var _voidElements = [
+    'area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen',
+    'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'
+];
+
 var Vis = function(obj, block) {
     return Vis.fn.init(obj, block);
 }
@@ -21,10 +26,46 @@ Vis.fn = Vis.prototype = {
 
     error: function(msg) {
         throw new Error(msg);
-    }
+    },
 };
 
 Vis.blocks = {};
+
+Vis.render = function(data) {
+    if (data instanceof Array) {
+        var html = '';
+        for (var i = 0; i < data.length; ++i) {
+            html += Vis.render(data[i]);
+        }
+        return html;
+    } else if (data instanceof Object) {
+        var tag = data.tag || 'div';
+        var attrs = data.attrs || {};
+        var html = "<" + tag;
+        if (data.cls || attrs['class']) {
+            var v = [];
+            data.cls && v.push(data.cls);
+            attrs['class'] && v.push(attrs['class']);
+            html += ' class="' + v.join(' ') + '"';
+        }
+        for (var attr in attrs) {
+            if (attr === 'class') {
+                continue;
+            }
+            if (attrs.hasOwnProperty(attr)) {
+                html += ' ' + attr + '="' + attrs[attr] + '"';
+            }
+        }
+        html += '>';
+        if (data.content || _voidElements.indexOf(tag) == -1) {
+            html += Vis.render(data.content || '');
+            html += '</' + tag + '>';
+        }
+        return html;
+    } else {
+        return data;
+    }
+}
 
 exports.Vis = Vis;
 

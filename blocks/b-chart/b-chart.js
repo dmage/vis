136e25812be3be2e99b36253478c52308dddc543
Oleg Bulatov <oleg@bulatov.me>
2013-03-13 11:42:43 +0400

    Keep created DOM objects for content.yAxes
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- blocks/b-chart/b-chart.js
+++ blocks/b-chart/b-chart.js
@@ -8,86 +8,81 @@ Vis.blocks['b-chart'] = Vis.extend(Vis.blocks['i-chart'], {
 
         this.__base.init(params);
 
-        this.initObject();
+        this.renderObjects();
     },
 
-    initObject: function() {
+    renderYAxes: function(pos, layout, method) {
         var _this = this,
             content = _this.content,
             yAxes = content.yAxes;
-        var i, l, yAxis;
 
-        this.$object.empty();
-
-        var mlc = [];
-        var ml = { tag: 'table', cls: 'b-layout', content: { tag: 'tr', content: mlc } };
-        for (i = 0, l = yAxes.length; i < l; ++i) {
-            yAxis = yAxes[i];
-            if (yAxis.pos !== 'left') {
+        var $tr = $('<tr>');
+        for (var i = 0, l = yAxes.length; i < l; ++i) {
+            var yAxis = yAxes[i];
+            if (yAxis.pos !== pos) {
                 continue;
             }
 
-            mlc.unshift({
+            var c = Vis.render({
                 tag: 'td',
                 cls: 'b-layout__cell',
                 content: { cls: 'b-box', content: "axis #" + i }
             });
-        }
-        ml = Vis.render(ml);
+            yAxis.$object = $(c);
 
-        var mrc = [];
-        var mr = { tag: 'table', cls: 'b-layout', content: { tag: 'tr', content: mrc } };
-        for (i = 0, l = yAxes.length; i < l; ++i) {
-            yAxis = yAxes[i];
-            if (yAxis.pos !== 'right') {
-                continue;
+            $tr[method](yAxis.$object);
+        }
+        layout.empty().append($tr);
+    },
+
+    renderObjects: function() {
+        this.$object.html(Vis.render([
+            {
+                cls: 'b-chart__header',
+                content: 'Header'
+            },
+            {
+                tag: 'table',
+                cls: 'b-9',
+                content: [
+                    {
+                        tag: 'tr',
+                        content: [
+                            { tag: 'td', cls: 'b-9__tl', content: 'tl' },
+                            { tag: 'td', cls: 'b-9__tc', content: 'tc' },
+                            { tag: 'td', cls: 'b-9__tr', content: 'tr' }
+                        ]
+                    },
+                    {
+                        tag: 'tr',
+                        content: [
+                            {
+                                tag: 'td',
+                                cls: 'b-9__ml',
+                                content: { tag: 'table', cls: 'b-layout' }
+                            },
+                            { tag: 'td', cls: 'b-9__mc', content: 'mc' },
+                            {
+                                tag: 'td',
+                                cls: 'b-9__mr',
+                                content: { tag: 'table', cls: 'b-layout' }
+                            }
+                        ]
+                    },
+                    {
+                        tag: 'tr',
+                        content: [
+                            { tag: 'td', cls: 'b-9__bl', content: 'bl' },
+                            { tag: 'td', cls: 'b-9__bc', content: 'bc' },
+                            { tag: 'td', cls: 'b-9__br', content: 'br' }
+                        ]
+                    }
+                ]
             }
+        ]));
 
-            mrc.push({
-                tag: 'td',
-                cls: 'b-layout__cell',
-                content: { cls: 'b-box', content: "axis #" + i }
-            });
-        }
-        mr = Vis.render(mr);
-
-        var c = '';
-        c += Vis.render({
-            cls: 'b-chart__header',
-            content: 'Header'
-        });
-        c += Vis.render({
-            tag: 'table',
-            cls: 'b-9',
-            content: [
-                {
-                    tag: 'tr',
-                    content: [
-                        { tag: 'td', cls: 'b-9__tl', content: 'tl' },
-                        { tag: 'td', cls: 'b-9__tc', content: 'tc' },
-                        { tag: 'td', cls: 'b-9__tr', content: 'tr' }
-                    ]
-                },
-                {
-                    tag: 'tr',
-                    content: [
-                        { tag: 'td', cls: 'b-9__ml', content: ml },
-                        { tag: 'td', cls: 'b-9__mc', content: 'mc' },
-                        { tag: 'td', cls: 'b-9__mr', content: mr }
-                    ]
-                },
-                {
-                    tag: 'tr',
-                    content: [
-                        { tag: 'td', cls: 'b-9__bl', content: 'bl' },
-                        { tag: 'td', cls: 'b-9__bc', content: 'bc' },
-                        { tag: 'td', cls: 'b-9__br', content: 'br' }
-                    ]
-                }
-            ]
-        });
-
-        this.$object.html(c);
+        this.renderYAxes('left', $('.b-9__ml .b-layout', this.$object), 'prepend');
+        this.renderYAxes('right', $('.b-9__mr .b-layout', this.$object), 'append');
     }
 });
 
